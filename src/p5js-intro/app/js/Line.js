5f74c350db02b9d8bd798858d211949bbadd6675
santi <santi@proper-code.com>
2021-10-21 00:03:07 -0700

    p5 build

---
+++ src/p5js-intro/app/js/Line.js
@@ -0,0 +1,46 @@
+class Line{
+    constructor( startPoint, endPoint, curveDetail = 20 ){
+        this.aPoint = startPoint
+        this.bPoint = endPoint
+        this.segmentLength = curveDetail
+        this.segmentsDrawn = 0
+        this.recurred = 0
+        this.computeSegments()
+    }
+
+    computeSegments(){
+        this.currentSegment = 0
+        this.curvePoints = [
+            [ this.aPoint.x, this.aPoint.y ],
+            [ this.aPoint.x + Math.cos( this.aPoint.v ) * 10, this.aPoint.y + Math.sin( this.aPoint.v ) * 10 ],
+            [ this.bPoint.x - Math.cos( this.bPoint.v ) * 10, this.bPoint.y - Math.sin( this.bPoint.v ) * 10 ],
+            [ this.bPoint.x, this.bPoint.y ]
+        ]
+
+        this.segments = []
+        for( var i = 0 ; i <= this.segmentLength ; i++ ) this.segments.push( this.getPointOnCurve( this.curvePoints, i / this.segmentLength ) )
+        this.segmentsDrawn++
+    }
+
+    getCurrentSegment(){
+        let p = this.segments[ this.currentSegment ]
+        let p2 = this.segments[ this.currentSegment + 1 ]
+        this.currentSegment++
+        return [ p, p2 ]
+    }
+
+    getPointOnCurve( points, t ) {
+        const len = points.length;
+        if (len === 1) return points[0]
+        else {
+          let p = []
+          for (let i = 0; i < len - 1; i++) {
+            const p1 = points[i], p2 = points[i + 1]
+            p.push([ (1 - t) * p1[0] + t * p2[0], (1 - t) * p1[1] + t * p2[1] ])
+          }
+          return this.getPointOnCurve( p, t )
+        }
+    }
+}
+
+export { Line as default }
---
+++ src/p5js-intro/app/js/main.js
@@ -0,0 +1,112 @@
+import p5 from 'p5'
+import portrait from './../assets/p1.png'
+import Line from './Line'
+
+class Renderer extends p5{
+    static redirectP5Callbacks( p ) { p.preload = p.preload.bind( p ) }
+
+    constructor( ){
+        super( Renderer.redirectP5Callbacks )
+        this.levels = 8
+        this.portraitSize = 200
+        this.density = 1
+        this.startLines = 1
+        this.minLineLength = 20
+        this.maxLineLength = 30
+        this.recurred = 0
+        this.recursionLevel = 5
+        this.curveDetail = 10
+        this.speed = 1
+        this.points = []
+        this.totalPoints = null
+        this.lines = []
+        this.levelData = new Array(this.levels).fill(0)
+        this.pointsToDraw = new Array(this.levels).fill(0)
+    }
+
+    preload( ){ this.img = this.loadImage( portrait ) }
+
+    setup( ){
+        let size = Math.min( window.innerWidth, window.innerHeight )
+        this.createCanvas( size, size )
+        this.getImagePoints()
+        this.pixelDensity( 2 )
+        this.strokeWeight( 0.25 )
+        
+        this.stroke( 255 )
+        for( var i = 0 ; i < this.startLines ; i++ ){
+            let aPoint = this.points.shift()
+            this.lines.push( new Line( aPoint, this.getNextPoint( aPoint) ) )
+        }
+    }
+
+    getImagePoints(){
+        this.pg = this.createGraphics(this.portraitSize, this.portraitSize);
+        this.pg.pixelDensity(1)
+        this.pg.image( this.img, 0, 0, this.pg.width, this.pg.height )
+        this.pg.filter(this.pg.POSTERIZE,this.levels)
+        this.pg.loadPixels()
+        this.pg.get()
+        this.pxls = []
+        for( let i = 0 ; i < this.pg.pixels.length ; i+= 4 ) {
+            this.levelData[ Math.round( ( this.pg.pixels[ i ] / 255 ) * ( this.levels - 1 ) ) ]++
+            this.pxls.push( this.pg.pixels[ i ] )
+        }
+        this.levelData.forEach( ( l, i ) => {
+            let pointsToAdd = l * (this.levels - i - 1 ) * this.density
+            let threshold = Math.round( i / ( this.levels - 1 ) * 255 )
+            while( pointsToAdd > 0 ){
+                let [ x, y ] = [ Math.random() * this.width, Math.random() * this.height ]
+                let xRemapped = Math.floor( ( x / this.width ) * this.portraitSize )
+                let yRemapped = Math.floor( ( y / this.height ) * this.portraitSize )
+                let pxl = this.pxls[ yRemapped * this.portraitSize + xRemapped ]
+                if( pxl == threshold ) {
+                    this.points.push( { x : x, y : y, v : Math.random() * Math.PI * 2 } )
+                    pointsToAdd--
+                }
+                this.totalPoints = this.points.length
+            }
+        })
+    }
+
+    getNextPoint( aPoint ){
+        let candidates = [], alternates = []
+        this.points.forEach( ( p, i ) => {
+            let [ x, y ] = [ p.x - aPoint.x, p.y - aPoint.y ]
+            var d = Math.sqrt( x * x + y * y )
+            if( d > this.minLineLength && d < this.maxLineLength ) candidates.push( i )
+            if( d > this.maxLineLength ) alternates.push( [ i, d ] )
+        })
+
+        let selectIndex = null
+            if( candidates.length ) selectIndex = candidates[ Math.floor( Math.random() * candidates.length ) ]
+            else selectIndex = alternates.sort( ( a, b ) => { return a[1]-b[1] } )[ 0 ][ 0 ]
+        
+        let bPoint = this.points[ selectIndex ]
+        this.points.splice( selectIndex, 1 )
+        return bPoint
+    }
+
+    draw(){
+        this.speed = Math.min( 10, Math.floor( ( 1 - this.points.length / this.totalPoints ) * 500 + 1 ) )
+        if( !this.points.length ) return
+        for( var i = 0 ; i < this.speed ; i++ ){
+            this.lines.forEach( l => {
+                if( l.currentSegment < l.segments.length - 1 ){
+                    let ps = l.getCurrentSegment()
+                    this.line( ps[ 0 ][ 0 ], ps[ 0 ][ 1 ], ps[ 1 ][ 0 ], ps[ 1 ][ 1 ] )
+                } else {
+                    l.aPoint = l.bPoint
+                    l.bPoint = this.getNextPoint( l.aPoint )
+                    l.computeSegments()
+                    if( this.recurred < this.recursionLevel && Math.random() > 0.8 ) {
+                        this.lines.push( new Line( l.aPoint, this.getNextPoint( l.aPoint ) ) )
+                        this.recurred++
+                    }
+                }
+            })
+        }
+    }
+}
+
+new Renderer()
